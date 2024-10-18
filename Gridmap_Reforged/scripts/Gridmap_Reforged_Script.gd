e4e7684728337da168e9aebfb46e8e84cc5e7d12
xXEINYXx <einycontactpro@gmail.com>
2024-10-18 19:30:45 +0200

    modification

--- Gridmap_Reforged/scripts/Gridmap_Reforged_Script.gd
+++ Gridmap_Reforged/scripts/Gridmap_Reforged_Script.gd
@@ -1,7 +1,11 @@
 @tool
 extends Node3D
 
-@export var floor: int = 0
+@export var floor:int = 0:
+	set(val):
+		if(val != floor):
+			floor = val
+			change_grid_Y()
 @export var instance: PackedScene 
 
 var gridMap_Reforged: Node3D  # Variable pour l'instance de GridMap_Reforged
@@ -57,39 +61,12 @@ func _enter_tree():
 	# Instancier GridMap_Reforged et l'ajouter comme enfant
 	gridMap_Reforged = GridMap_Reforged.instantiate() as Node3D
 	add_child(gridMap_Reforged)
-	print("GridMap_Reforged ajouté")
-	
+	print("GridMap_Reforged ajouté")	
 
 func _exit_tree():
 	# Libérer l'instance de GridMap_Reforged lorsqu'elle est supprimée de l'arbre
 	if gridMap_Reforged:
 		gridMap_Reforged.queue_free()
-
-func _input(event):
-	if event.is_action_just_pressed("clic_gauche"):
-		if Engine.is_editor_hint():
-		#Comparer la valeur précédente de 'floor' avec la nouvelle
-			if floor != previous_floor:
-				previous_floor = floor
-				change_grid_Y()
-
-				#MOUSE POSITION IN THE EDITOR
-
-				var mouse_position: Vector2 = EditorInterface.get_editor_viewport_3d().get_mouse_position()
-				var editor_viewport = EditorInterface.get_editor_viewport_3d().get_visible_rect()
-
-				if editor_viewport.has_point(mouse_position):
-					var origin: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_origin(mouse_position)
-					var direction: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_normal(mouse_position)
-					var distance = 1000
-					var world3D = get_world_3d().direct_space_state
-					var RayQuery = PhysicsRayQueryParameters3D.create(origin,direction*distance)
-					var result = world3D.intersect_ray(RayQuery)
-					if result:
-						mesh_to_place = instance.instantiate()
-						mesh_to_place.position = Vector3(result.position.x,result.position.y,result.position.z)
-						add_child(mesh_to_place)
-						print("Block placed")
 
 # Change la position en Y du grid_map
 func change_grid_Y():
