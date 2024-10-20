fdbeffab6b537fb13132ecd52ab359ac8b70d9b5
xXEINYXx <einycontactpro@gmail.com>
2024-10-20 18:46:24 +0200

    push

--- Gridmap_Reforged/scripts/Gridmap_Reforged_Script.gd
+++ Gridmap_Reforged/scripts/Gridmap_Reforged_Script.gd
@@ -22,44 +22,45 @@ var mesh_to_place
 var GridCoord 
 var GridDisplay
 
+func shootRayFromMouse():
+	var mouse_position: Vector2 = EditorInterface.get_editor_viewport_3d().get_mouse_position()
+	var editor_viewport = EditorInterface.get_editor_viewport_3d().get_visible_rect()
+	if !editor_viewport.has_point(mouse_position):
+		return null
+	var distance = 1000
+	var origin = EditorInterface.get_editor_viewport_3d().get_camera_3d().position
+	var endpoint = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_position(mouse_position, distance)
+	var world3D = get_world_3d().direct_space_state
+	#var RayQuery = PhysicsRayQueryParameters3D.create(origin,direction*distance)
+	var RayQuery = PhysicsRayQueryParameters3D.create(origin,endpoint)
+	var result = world3D.intersect_ray(RayQuery)
+	if result:
+		if "position" in result:
+			return result.get("position")
+	else:
+		return null
+
 func MouseMoved(event):
 	self.position = Vector3(0,0,0)
 	self.rotation = Vector3(0,0,0)
 	#this is not a good idea really, we want to be able to put the gridmap anywhere and still have it behave right
-	var mouse_position: Vector2 = EditorInterface.get_editor_viewport_3d().get_mouse_position()
-	var editor_viewport = EditorInterface.get_editor_viewport_3d().get_visible_rect()
-	var origin: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_origin(mouse_position)
-	var direction: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_normal(mouse_position)
-	var distance = 1000
-	var world3D = get_world_3d().direct_space_state
-	var RayQuery = PhysicsRayQueryParameters3D.create(origin,direction*distance)
-	var result = world3D.intersect_ray(RayQuery)
-	if result:
-		if "position" in result:
-			var result_position = result.get("position")
-			var result_position_slice_x = result_position[0]
-			var result_position_slice_z = result_position[2]
-			GridDisplay.position.x = floor(result_position_slice_x)
-			GridDisplay.position.z = floor(result_position_slice_z)
+	var rp = shootRayFromMouse()
+	if rp is Vector3:
+		GridDisplay.position.x = rp[0]
+		GridDisplay.position.z = rp[2]
 	
 func ClickHappened(event):
 	var mouse_position: Vector2 = EditorInterface.get_editor_viewport_3d().get_mouse_position()
 	var editor_viewport = EditorInterface.get_editor_viewport_3d().get_visible_rect()
-	if editor_viewport.has_point(mouse_position):
-		var origin: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_origin(mouse_position)
-		var direction: Vector3 = EditorInterface.get_editor_viewport_3d().get_camera_3d().project_ray_normal(mouse_position)
-		var distance = 1000
-		var world3D = get_world_3d().direct_space_state
-		var RayQuery = PhysicsRayQueryParameters3D.create(origin,direction*distance)
-		var result = world3D.intersect_ray(RayQuery)
-		if result:
-			mesh_to_place = instance1.instantiate()
-			mesh_to_place.position = Vector3(floor(0.5 + result.position.x/GridCoord.cell_size)*GridCoord.cell_size,result.position.y,floor(0.5 + result.position.z/GridCoord.cell_size)*GridCoord.cell_size)
-			#make it unclickable on the editor
-			mesh_to_place.set_meta("_edit_lock_", true)
-			add_child(mesh_to_place,false,Node3D.INTERNAL_MODE_BACK)
-			mesh_to_place.set_owner(get_tree().get_edited_scene_root())
-			print("Block placed")
+	var rp = shootRayFromMouse()
+	if rp is Vector3:
+		mesh_to_place = instance1.instantiate()
+		mesh_to_place.position = Vector3(floor(0.5 + rp.x/GridCoord.cell_size)*GridCoord.cell_size,rp.y,floor(0.5 + rp.z/GridCoord.cell_size)*GridCoord.cell_size)
+		#make it unclickable on the editor
+		mesh_to_place.set_meta("_edit_lock_", true)
+		add_child(mesh_to_place,false,Node3D.INTERNAL_MODE_BACK)
+		mesh_to_place.set_owner(get_tree().get_edited_scene_root())
+		print("Block placed")
 
 # Appelée lorsque le noeud entre dans l'arbre de la scène pour la première fois
 func _ready():
